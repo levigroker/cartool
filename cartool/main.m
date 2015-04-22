e81556f44a55fd73bcfc7fab6c80f3c14e4574d0
Levi Brown <levigroker@gmail.com>
2015-04-21 18:03:19 -0600

    More argument fixes. Deriving exec name.

--- cartool/main.m
+++ cartool/main.m
@@ -4,10 +4,11 @@
 //
 //  Created by Steven Troughton-Smith on 14/07/2013.
 //  Copyright (c) 2013 High Caffeine Content. All rights reserved.
+//  Modified by Levi Brown <mailto:levigroker@gmail.com> April 21, 2015
 //
 
 #import <Foundation/Foundation.h>
-
+#include <libproc.h>
 
 @interface CUICommonAssetStorage : NSObject
 
@@ -60,35 +61,35 @@ void CGImageWriteToFile(CGImageRef image, NSString *path)
     CFURLRef url = (__bridge CFURLRef)[NSURL fileURLWithPath:path];
     CGImageDestinationRef destination = CGImageDestinationCreateWithURL(url, kUTTypePNG, 1, NULL);
     CGImageDestinationAddImage(destination, image, nil);
-	
+    
     if (!CGImageDestinationFinalize(destination)) {
         NSLog(@"Failed to write image to %@", path);
     }
-	
+    
     CFRelease(destination);
 }
 
 
 void exportCarFileAtPath(NSString * carPath, NSString *outputDirectoryPath)
 {
-	NSError *error = nil;
-	
-	outputDirectoryPath = [outputDirectoryPath stringByExpandingTildeInPath];
-
-	CUIThemeFacet *facet = [CUIThemeFacet themeWithContentsOfURL:[NSURL fileURLWithPath:carPath] error:&error];
-	
-	CUICatalog *catalog = [[CUICatalog alloc] init];
-	
-	/* Override CUICatalog to point to a file rather than a bundle */
-	[catalog setValue:facet forKey:@"_storageRef"];
-	
-	/* CUICommonAssetStorage won't link */
-	CUICommonAssetStorage *storage = [[NSClassFromString(@"CUICommonAssetStorage") alloc] initWithPath:carPath];
-	
-	for (NSString *key in [storage allRenditionNames])
-	{
-		printf("%s\n", [key UTF8String]);
-
+    NSError *error = nil;
+    
+    outputDirectoryPath = [outputDirectoryPath stringByExpandingTildeInPath];
+    
+    CUIThemeFacet *facet = [CUIThemeFacet themeWithContentsOfURL:[NSURL fileURLWithPath:carPath] error:&error];
+    
+    CUICatalog *catalog = [[CUICatalog alloc] init];
+    
+    /* Override CUICatalog to point to a file rather than a bundle */
+    [catalog setValue:facet forKey:@"_storageRef"];
+    
+    /* CUICommonAssetStorage won't link */
+    CUICommonAssetStorage *storage = [[NSClassFromString(@"CUICommonAssetStorage") alloc] initWithPath:carPath];
+    
+    for (NSString *key in [storage allRenditionNames])
+    {
+        printf("%s\n", [key UTF8String]);
+        
         NSArray *images = [catalog imagesWithName:key];
         for( CUINamedImage *image in images )
         {
@@ -116,20 +117,33 @@ void exportCarFileAtPath(NSString * carPath, NSString *outputDirectoryPath)
                     CGImageWriteToFile(cgImage, [outputDirectoryPath stringByAppendingPathComponent:name]);
             }
         }
-	}
+    }
 }
 
 int main(int argc, const char * argv[])
 {
-	@autoreleasepool {
-	    
-		if (argc < 2)
-		{
-			printf("Usage: cartool <oath to Assets.car> [outputDirectory]\n");
-			return -1;
-		}
-	    
-		exportCarFileAtPath([NSString stringWithUTF8String:argv[1]], argc > 2 ? [NSString stringWithUTF8String:argv[2]] : nil);
-	}
+    @autoreleasepool {
+        
+        if (argc < 2 || argc > 3)
+        {
+            //Default executable name
+            NSString *execName = @"cartool";
+
+            //Attempt to determine executable name from executable path
+            char pathbuf[PROC_PIDPATHINFO_MAXSIZE];
+            int res = proc_pidpath(getpid(), pathbuf, sizeof(pathbuf));
+            if (res > 0)
+            {
+                //We have the full path, so we'll take the last path component as our executable name
+                NSString *path = [NSString stringWithUTF8String:pathbuf];
+                execName = [path lastPathComponent];
+            }
+
+            printf("Usage: %s <car_file> [output_directory]\n", [execName cStringUsingEncoding:NSUTF8StringEncoding]);
+            return -1;
+        }
+        
+        exportCarFileAtPath([NSString stringWithUTF8String:argv[1]], argc > 2 ? [NSString stringWithUTF8String:argv[2]] : nil);
+    }
     return 0;
 }
